503d752483f711cacd5fde6e7b24e7f3d6aa66bb
Prof. Savio Saldanha <consultsavio@gmail.com>
2023-10-05 20:18:37 +0530

    text to write and wide config

--- 1_Home.py
+++ 1_Home.py
@@ -1,12 +1,13 @@
 
 import streamlit as st
 
+st.set_page_config(layout='wide')
 
 st.header("Vaishali Rangari - Business Analyst")
 
 col1, col2 = st.columns([1,2])
 col1.image("business analyst badge.png", use_column_width=True)
-col2.text("This site serves as a testament to the business analysis skills exhibited by Vaishali Rangari. \
+col2.write("This site serves as a testament to the business analysis skills exhibited by Vaishali Rangari. \
           \n\nShe possesses business analyst work experience. This site was built by Vaishali as a business analyst and the educator team at \
           \n\nSavio Education Global as part of her business analyst work experience. \
           \n\nHer work included creating the BRD, FRD, User Stories, Data Model, Wireframe, Data Visualization using Tableau, Test Scenarios, Test Cases, Defect Reports and working with Atlassian JIRA.\
